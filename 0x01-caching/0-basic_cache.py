6c31b2248027efd743fea886f96af0eaf0e2da78
randaaz <randaaziz972@gmail.com>
2024-06-25 02:17:11 -0700

    0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" This module contains the BasicCache class that inherits from BaseCaching. """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class"""
+
+    def put(self, key, item):
+        """Add an item in the cache."""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key."""
+        return self.cache_data.get(key, None)
